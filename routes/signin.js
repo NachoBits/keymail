73d4becce53c95fe5b1da3ec870a202b61d34f36
Aaron Griffin <aig787@gmail.com>
2014-11-02 02:11:53 -0800

    Implemented client side crypto and session key generation

--- routes/signin.js
+++ routes/signin.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('signin.ejs', { title: 'Express' });
 });
 
+router.post('/', function(req, res) {
+  res.send(req.protocol + "://" + req.get('Host'));
+});
+
 module.exports = router;
